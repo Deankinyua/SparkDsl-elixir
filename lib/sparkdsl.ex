485ab4d1192fdc697c1740f205db3754b010c2e3
deankinyua <kinyuadean@gmail.com>
2024-06-01 20:04:27 +0300

    successfully added ash which comes with spark, reactor, ecto out of the box

--- lib/sparkdsl.ex
+++ lib/sparkdsl.ex
@@ -1,18 +1,52 @@
 defmodule Sparkdsl do
   @moduledoc """
   Documentation for `Sparkdsl`.
+  ## Example
+
+  ```
+  quiz "English end of term exam", :essay do
+
+    question "Jane Eyre, discuss."
+
+  end
+
+  quiz "English end of term exam", :essay do
+
+    question "1 + 1", "2"
+    multi_choice_question "3 * 9" do
+      answer :a
+      choice :a
+      choice :b
+      choice :c
+      choice :d
+    end
+  end
+  ```
+
   """
 
-  @doc """
-  Hello world.
+  defmodule Quiz do
+    defstruct [:name, :type, :questions]
 
-  ## Examples
+    def introspect(quiz) do
+      """
+      #{quiz.name} (#{quiz.type})
 
-      iex> Sparkdsl.hello()
-      :world
+      Question(s):
+      #{Enum.map_join(quiz.questions, "\n", & &1.title)}
 
-  """
-  def hello do
-    :world
+      """
+    end
+  end
+
+  defmodule Question do
+    defstruct [:title]
+  end
+
+  defmodule Dsl do
+    @question %Spark.Dsl.Entity{
+      name: :question,
+      describe: "Define a question in a quiz"
+    }
   end
 end
